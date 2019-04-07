dc01c3bd422bd430d69fb6c63fbe6300971a2b83
VentGrey <omarpurataf@gmail.com>
2019-04-07 14:49:31 -0500

    Added doom emacs files

---
+++ .doom.d/packages.el
@@ -0,0 +1,17 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+;; SRCERY para que se vea perrón
+(package! srcery-theme)
+(package! ghub)
+(package! org-notebook)
+(package! spacemacs-theme)
+
+
+;; Quitar paquetes culeros
+(package! treemacs :disable t)
